0a6a26fd21f0706124959502f640b442f35e0fa2
shi-design <shivanisundararamoorthy16@gmail.com>
2025-10-02 15:49:05 +0530

    Fix server.js duplicate route

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users', require('./routes/users'));
 
 // Base route for testing
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('SpendWise API is running...');
 });
 
 // Port setup
@@ -30,7 +30,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-app.get('/', (req, res) => {
-  res.send('SpendWise API is running...');
 });
